88c4543945658c625a6a6655aad4ae417c9b3edf
Sebastian Clancy <foldit.com@gmail.com>
2019-06-24 23:45:29 -0700

    figured out the history() async iterator

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import markovify
 import discord
 client = discord.Client()
 file = open('./token.txt', "r")
@@ -11,9 +12,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("p!"):
-        message.content = message.content[2:]
-    print(message.content);
+    if not message.content.startswith("p!"):
+        return
+    if message.author == client.user:
+            return
+    if message.author.bot: return
+    message.content = message.content[2:]
+    message.content = message.content.split(" ")
+    if message.content[0]=="past":
+        async for m in message.channel.history(limit=5):
+            await message.channel.send(m.content)
+
+    await message.channel.send(message.content)
+
+    
 
 
 
