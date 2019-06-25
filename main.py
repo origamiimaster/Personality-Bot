f817a9f75c3f5b136f4b6c7081e18cb0d320fac4
Sebastian Clancy <foldit.com@gmail.com>
2019-06-24 22:11:49 -0700

    fixing merge conflict cause I am dumb

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import discord
 client = discord.Client()
+file = open('./token.txt', "r")
+token = file.read()
+file.close()
 
 
 @client.event
 async def on_ready():
     print("Bot is ready")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("p!"):
+        message.content = message.content[2:]
+    print(message.content);
+
 
 
 
