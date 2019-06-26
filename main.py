fef798cdf79752278cb12f4cb0707c6130369a54
Sebastian Clancy <foldit.com@gmail.com>
2019-06-26 09:51:20 -0700

    new method for counting messages

--- main.py
+++ main.py
@@ -23,17 +23,29 @@ async def on_message(message):
         async for m in message.channel.history(limit=5):
             await message.channel.send(m.content)
     if message.content[0]=="mimic":
+        s = message.content[1]
+        message.content[1] = ''.join(filter(lambda x: x.isdigit(), s))
         text = ""
+        counter=0
         async for m in message.channel.history(limit=None):
             if m.author.id == int(message.content[1]):
                 if not m.content.startswith("p!"):
-                    text = text +m.content+". "
+                    if not m.content == "":
+                        text = text +m.content+"."
+                        counter =counter +1
+            if counter >=300:
+                break
+        print(text)
         text_model = markovify.Text(text)
-        
-
+        sentence = ""
+        while sentence=="" or sentence ==None:
+            sentence = text_model.make_sentence()
+            print(sentence)
+            if sentence != "" and sentence !=None:
+                break
         #print(text)
         #await message.channel.send(text)
-        await message.channel.send(text_model.make_sentence())
+        await message.channel.send(sentence)
     if message.content[0]=="jfk":
         file = open('./jfk.txt')
         text = file.read()
@@ -41,5 +53,18 @@ async def on_message(message):
         text_model = markovify.Text(text)
 
         await message.channel.send(text_model.make_sentence())
+    if message.content[0]=="help":
+        embed=discord.Embed(title="Commands", description="Here are the commands:", color=0xFFFF00)
 
+        embed.add_field(name="mimic", value="markov chains a sentence for a mentioned user, based off of past messages in the channel", inline= True)
+        embed.add_field(name="jfk", value="markov chains a sentence from President John Fitzgerald Kennedy's speeches", inline= True)
+
+
+        await message.channel.send(embed=embed)            
+
+    
+        #message.channel.send({embed: {
+          #color: 3447003,
+          #description: "A very simple Embed!"
+        #}});
 client.run(token)
