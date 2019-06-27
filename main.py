f51eec6c1462de1a259f9cc5e9dbd4ca5a7a3e91
Will Done <willdone@Dawns-MacBook-Pro.local>
2019-06-27 13:44:18 -0700

    Added support for some of President Obama's speeches. Improved server-side logging capabilities.

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
+print("[ INFO ]  Bot initializing.")
 import markovify
+print("[ INFO ]  Successfully imported markovify module.")
 import discord
+print("[ INFO ]  Successfully imported discord module.")
 client = discord.Client()
 file = open('./token.txt', "r")
 token = file.read()
+print("[ INFO ]  Successfully processed discord bot token.")
 file.close()
-
 
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print("[ INFO ]  Bot ready; Listening for incoming messages.")
 
 @client.event
 async def on_message(message):
     if not message.content.startswith("p!"):
         return
     if message.author == client.user:
-            return
+        return
     if message.author.bot: return
     message.content = message.content[2:]
     message.content = message.content.split(" ")
-    if message.content[0]=="past":
-        async for m in message.channel.history(limit=5):
-            await message.channel.send(m.content)
     if message.content[0]=="mimic":
         s = message.content[1]
         message.content[1] = ''.join(filter(lambda x: x.isdigit(), s))
@@ -50,11 +50,13 @@ async def on_message(message):
             if sentence != "" and sentence !=None:
                 break
             if counter > 500:
-                sentence = "FAILED"
+                sentence = "ERROR: Sentence generation failure."
+                print ("[ FAIL ]  Sentence generation failure.")
                 break
         #print(text)
         #await message.channel.send(text)
         await message.channel.send(sentence)
+        print ("[ INFO ]  Mimic sentence generation for target user "+message.content[1]+" successful.")
     if message.content[0]=="jfk":
         file = open('./jfk.txt')
         text = file.read()
@@ -62,6 +64,7 @@ async def on_message(message):
         text_model = markovify.Text(text)
 
         await message.channel.send(text_model.make_sentence())
+        print ("[ INFO ]  JFK sentence generation successful.")
 
     if message.content[0]=="trumpTweets":
         file = open('./trumpTweets.txt')
@@ -70,16 +73,26 @@ async def on_message(message):
         text_model = markovify.Text(text)
 
         await message.channel.send(text_model.make_sentence())
+        print ("[ INFO ]  TrumpTweets sentence generation successful.")
+
+    if message.content[0]=="obama":
+        file = open('./obama.txt')
+        text = file.read()
+        file.close()
+        text_model = markovify.Text(text)
+
+        await message.channel.send(text_model.make_sentence())
+        print ("[ INFO ]  Obama sentence generation successful.")
 
     if message.content[0]=="help":
         embed=discord.Embed(title="Commands", description="Here are the commands:", color=0xFFFF00)
 
-        embed.add_field(name="mimic", value="markov chains a sentence for a mentioned user, based off of past messages in the channel", inline= True)
-        embed.add_field(name="jfk", value="markov chains a sentence from President John Fitzgerald Kennedy's speeches", inline= True)
-        embed.add_field(name="trumpTweets", value="markov chains a sentence from President Donald John Trump's Tweets", inline= True)
- 
-        await message.channel.send(embed=embed)            
+        embed.add_field(name="mimic", value="Markov chains a sentence for a mentioned user, based off of past messages in the channel", inline= True)
+        embed.add_field(name="jfk", value="Markov chains a sentence from President John Fitzgerald Kennedy's speeches", inline= True)
+        embed.add_field(name="trumpTweets", value="Markov chains a sentence from President Donald John Trump's Tweets", inline= True)
+        embed.add_field(name="obama", value="Markov chains a sentence from President Barrack Hussein Obama's Tweets", inline= True)
 
+        await message.channel.send(embed=embed)
     
         #message.channel.send({embed: {
           #color: 3447003,
