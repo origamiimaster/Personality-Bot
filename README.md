9fc201e0766cabe1a4aceced5cffc739ddd5286e
Sebastian Clancy <foldit.com@gmail.com>
2019-06-28 22:45:01 -0700

    Committing with a better readme and more error message fixes

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Personality-Bot
 Discord Bot for the Discord Hack Week
 
-# THE IDEA
+# The Idea
 
-This bot uses markovify to act out as users, without directly copying users.  
+This bot uses markovify to act out as users, without directly copying users.
+
+In addition, the bot can mimic any set of text provided in the personality folder. 
+
+Some test examples have been provided. 
+
+# Setup
+
+First, make sure that you have the correctly installed Python3, this was tested with 3.7.3
+
+Next, install the dependencies with pip, which are discord.py and markovify.  
+
+The bot token should be pasted within a file labeled "token.txt", located in the main directory.  
+
+Finally, run the main.py file to begin the bot.  
+
+# Adding additional personalities 
+
+Take the example text of whoever you wish to mimic, with punctuation separating sentences. 
+
+The more text there is, the more success the bot will have with generating satisfactory responses.  
+
+However, large files may take longer for the bot to initialize.  
+
+# The Future
+
+Eventually, this or a similar bot will be created using more ai learning techniques to mimic historic debates between historic figures and characters of fantasy.  
+
+Imagine Epic Rap Battles Of History, but with any two characters there is text written about.  
+
+Even though this is still a far off prospect, Personality-Bot is an important step in the right direction. 
+
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("[ INFO ]  Bot initializing.")
 import markovify
 print("[ INFO ]  Successfully imported markovify module.")
 import os.path
-print("[ INFO ] Successfully imported path")
+print("[ INFO ]  Successfully imported path")
 import discord
 print("[ INFO ]  Successfully imported discord module.")
 client = discord.Client()
@@ -11,7 +11,6 @@ token = file.read()
 print("[ INFO ]  Successfully processed discord bot token.")
 file.close()
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
 
 def listdir_nohidden(path):
         for f in os.listdir(path):
@@ -19,26 +18,22 @@ def listdir_nohidden(path):
                 yield f
 commandList = []
 
-
-
-
 @client.event
 async def on_ready():
     print ("[ INFO ]  Bot connected to Discord.")
     print ("[ INFO ]  Bot ready; Listening for incoming messages.")    
     await client.change_presence (activity=discord.Game(name='with your words'))
-    print("[ INFO ] GRABBING PERSONALITIES")
+    print("[ INFO ]  GRABBING PERSONALITIES")
     personality_path = os.path.join(dir_path, os.listdir(dir_path)[2])
     personality_list = listdir_nohidden(personality_path)
     for p in personality_list:
         path = os.path.join(personality_path,p)
         file = open(path, "r")
-        print(p[:-4])
+        print("[ INFO ]  Loading: " +p[:-4])
         commandList.append(p[:-4])
         globals()[p[:-4]] = markovify.Text(file.read())
-        #print(globals()[p[:4]].make_sentence())
         file.close()
-    #print(jfk.make_sentence())
+    print("[ INFO ]  Completed loading personality folder")
 @client.event
 async def on_message(message):
     if not message.content.startswith("p!"):
@@ -55,7 +50,6 @@ async def on_message(message):
         counter=0
         async for m in message.channel.history(limit=2500):
             if counter < 500:
-
                 if m.author.id == int(message.content[1]):
                     if not m.content.startswith("p!"):
                         if not m.content == "":
@@ -65,30 +59,38 @@ async def on_message(message):
                             else:
                                 text = text +m.content+". "
                                 counter =counter +1
-        #print(text)
         text_model = markovify.Text(text)
         sentence = ""
         counter = 0
         while sentence=="" or sentence ==None:
             counter =counter +1
             sentence = text_model.make_sentence()
-            #print(sentence)
             if sentence != "" and sentence !=None:
                 break
             if counter > 500:
                 sentence = "ERROR: Sentence generation failure."
                 print ("[ FAIL ]  Sentence generation failure.")
                 break
-        #print(text)
-        #await message.channel.send(text)
         await message.channel.send(sentence)
         print ("[ INFO ]  Mimic sentence generation for target user "+message.content[1]+" successful.")
     if message.content[0] == "quote":
-        string = message.content[1]+".make_sentence()"
-        embed=discord.Embed(title="Quoting "+message.content[1], description=eval(string), color=0x00FF00)
-        await message.channel.send(embed=embed)
-        
-
+        if len(message.content) <= 1:
+            string = "Try adding one of these options: "
+            for c in commandList:
+                string += c +", "
+            await message.channel.send(string+"or try the mimic command")
+        else:
+            if not message.content[1] in globals():
+                await message.channel.send("That is not a valid option for this command")
+            else:
+                string = message.content[1]+".make_sentence()"
+                text = eval(string)
+                if text =="" or text == None:
+                        embed=discord.Embed(title="Quoting "+message.content[1], description="There was an error generating a sentence, please try again or with another user", color=0xFF0000)
+                embed=discord.Embed(title="Quoting "+message.content[1], description=eval(string), color=0x00FF00)
+                await message.channel.send(embed=embed)
+        if len(message.content) > 2:
+            await message.channel.send("Just a reminder, this command currently only works with one input.")
     if message.content[0]=="help":
         embed=discord.Embed(title="Commands", description="Here are the commands:", color=0xFFFF00)
         for command in commandList:
@@ -96,13 +98,6 @@ async def on_message(message):
         embed.add_field(name="mimic", value="Markov chains a sentence for a mentioned user, based off of past messages in the channel", inline= True)
         await message.channel.send(embed=embed)
 
-        
-    if message.content[0]=="yugioh":
-        file = open('./'+message.content[1]+'.txt')
-        text = file.read()
-        file.close()
-        text_model = markovify.Text(text)
-        await message.channel.send(text_model.make_sentence())
 client.run(token)
 
 
